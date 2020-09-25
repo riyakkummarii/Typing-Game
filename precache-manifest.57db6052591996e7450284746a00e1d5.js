0362952b7aab3418506dbadc7c3fd256f54bac2b
Riya Kumari <riyakkummarii@gmail.com>
2020-09-25 11:06:30 +0530

    Updates

---
+++ precache-manifest.57db6052591996e7450284746a00e1d5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9c61cd446cd2437f5acb9dd9523470",
+    "url": "/Typing-Game/index.html"
+  },
+  {
+    "revision": "bf1364fb6d7eabc7a460",
+    "url": "/Typing-Game/static/js/2.bef16793.chunk.js"
+  },
+  {
+    "revision": "da603dfcb2ecf737cd28",
+    "url": "/Typing-Game/static/js/main.d63ad8e8.chunk.js"
+  },
+  {
+    "revision": "d219917b6870601a51ec",
+    "url": "/Typing-Game/static/js/runtime-main.5c05e870.js"
+  }
+]);
